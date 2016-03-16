6efd41568e20563e5475f14ab8c02555eee3cf47
Wojciech Gawronski <afronski@gmail.com>
2016-03-16 17:54:04 +0100

    Removing unnecessary comments.

--- interactive_tetris/config/dev.exs
+++ interactive_tetris/config/dev.exs
@@ -1,11 +1,5 @@
 use Mix.Config
 
-# For development, we disable any cache and enable
-# debugging and code reloading.
-#
-# The watchers configuration can be used to run external
-# watchers to your application. For example, we use it
-# with brunch.io to recompile .js and .css sources.
 config :interactive_tetris, InteractiveTetris.Endpoint,
   http: [port: 4000],
   debug_errors: true,
@@ -13,7 +7,6 @@ config :interactive_tetris, InteractiveTetris.Endpoint,
   check_origin: false,
   watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin"]]
 
-# Watch static and templates for browser reloading.
 config :interactive_tetris, InteractiveTetris.Endpoint,
   live_reload: [
     patterns: [
@@ -24,15 +17,9 @@ config :interactive_tetris, InteractiveTetris.Endpoint,
     ]
   ]
 
-# Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
-
-# Set a higher stacktrace during development.
-# Do not configure such in production as keeping
-# and calculating stacktraces is usually expensive.
 config :phoenix, :stacktrace_depth, 20
 
-# Configure your database
 config :interactive_tetris, InteractiveTetris.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
