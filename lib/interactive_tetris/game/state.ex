4469d15e142e394e1885d915eb501970fb0d703b
Michal Muskala <michal@muskala.eu>
2016-04-09 09:39:13 +0200

    Prepare state task

--- lib/interactive_tetris/game/state.ex
+++ lib/interactive_tetris/game/state.ex
@@ -11,12 +11,8 @@ defmodule InteractiveTetris.Game.State do
   defstruct [:board, :next, :current, :rotation, :x, :y, :points, :room, :active]
 
   def cells_for_shape(state) do
-    shape = Shapes.get(state.current, state.rotation)
-    for {row, row_i} <- Enum.with_index(shape) do
-      for {col, col_i} <- Enum.with_index(row), col != 0 do
-        {col_i + state.x, row_i + state.y}
-      end
-    end |> List.flatten
+    shape = # ...
+    # get_cells
   end
 
   def cell_at(state, {x, y}) do
