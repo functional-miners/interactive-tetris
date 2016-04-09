1bd202f0268495183e80b6d2572223871ff21b01
Michal Muskala <michal@muskala.eu>
2016-04-09 07:27:35 +0200

    Prepare rotator task

--- lib/interactive_tetris/rotator.ex
+++ lib/interactive_tetris/rotator.ex
@@ -1,35 +1,18 @@
 defmodule InteractiveTetris.Rotator do
   import Enum, only: [at: 2, count: 1]
 
-  def rotate(piece, 0), do: piece
-
-  def rotate(piece, 90) do
-    rotated_width = count(piece)
-    rotated_height = count(hd(piece))
-    for col <- (0 .. rotated_height - 1), into: [] do
-      for row <- (rotated_width - 1 .. 0), into: [] do
-        piece |> at(row) |> at(col)
-      end
-    end
-  end
-
-  def rotate(piece, 180) do
+  def rotate(piece, 0) do
     piece
-    |> rotate(90)
-    |> rotate(90)
   end
 
-  def rotate(piece, 270) do
-    piece
-    |> rotate(180)
-    |> rotate(90)
-  end
-
-  def rotate(piece, amount) when rem(amount, 90) == 0 do
-    piece |> rotate(rem(amount, 360))
+  def rotate(piece, angle) do
+    # ...
   end
 
   def rotate(_, _) do
     raise ArgumentError, "Amount must be divisible by 90"
   end
+
+  defp width(piece),  do: count(piece)
+  defp height(piece), do: count(hd(piece))
 end
